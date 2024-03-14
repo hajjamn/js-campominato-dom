7fc1079e20c56ccdc196decafa29a98eec26e190
hajjamn <hajjamn@gmail.com>
2024-03-14 11:52:53 +0100

    ADD: bombs array generation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+/* FUNZIONI */
+
 function generateGrid() {
   //Dichiaro il contenitore dove andro' a mettere le celle
   const containerElement = document.querySelector('.game-container');
-
 
   //Dichiaro l'elemento del selezionatore di difficolta'
   const difficultySelectorElement = document.getElementById('difficulty');
@@ -14,32 +15,48 @@ function generateGrid() {
   containerElement.classList.remove('difficulty-10', 'difficulty-9', 'difficulty-7')
   containerElement.innerHTML = '';
 
+  //Dichiaro l'array delle bombe e il numero delle bombe
+  const bombNumber = 16;
+  const bombs = [];
+
+  //Genero numeri casuali da 1 a lunghezza del lato alla seconda e se non sono gia' presenti nell'array delle bombe ce li inserisco finche l'array delle bombe non e' lungo bombNUmber 
+  while (bombs.length < 16) {
+    let bombCellNumber = Math.floor(Math.random() * bombNumber) + 1;
+    if (bombs.includes(bombCellNumber) == false) {
+      bombs.push(bombCellNumber)
+    }
+  }
+  console.log(bombs)
+
   //Genero tutto in base alla dificolta'
   for (let i = 0; i < Math.pow(squareSideLength, 2); i++) {
+
+    //Dichiaro contatore di caselle
     const num = i + 1;
+
+    //Definisco la difficolta' aggiungendo la classe al container del gioco
     containerElement.classList.add(`difficulty-${squareSideLength}`)
 
+    //Aggiungo un div, gli do la classe cell, ci metto dentro il numero della sua casella e lo appendo al container
     const cellElement = document.createElement('div');
     cellElement.classList.add('cell');
     cellElement.innerHTML = num
-
     containerElement.append(cellElement);
-
 
     //Aggiungo la funzionalita' di cambio colore alle celle
     cellElement.addEventListener('click', function () {
-      this.classList.toggle('bg-secondary')
+      this.classList.toggle('bg-primary')
       console.log(`Hai cliccato la casella ${num}`)
     });
   }
 }
 
+/* CODICE */
 
 //dicahiaro il bottone di generazione della griglia
 const playButton = document.querySelector('.start');
 
 //aggiongo l'event listener al bottone
-
 game.addEventListener('submit', function(event) {
   event.preventDefault();
   generateGrid();
