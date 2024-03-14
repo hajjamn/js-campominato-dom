c0a19eef62c4a39a4e8efceba974856b972bd294
hajjamn <hajjamn@gmail.com>
2024-03-14 13:11:32 +0100

    FIX: minor issues

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ function generateGrid() {
   console.log(`Ho cliccato il bottone con difficolta' ${squareSideLength}`)
   containerElement.classList.remove('difficulty-10', 'difficulty-9', 'difficulty-7')
   containerElement.innerHTML = '';
+  isGameOver = false;
 
   //Dichiaro l'array delle bombe e il numero delle bombe
   const bombNumber = 16;
@@ -32,7 +33,8 @@ function generateGrid() {
   let score = 0;
   let maxScore = Math.pow(squareSideLength, 2) - bombNumber;
 
-  //Genero un array di caselle cliccate
+  //Genero un array di caselle cliccate e di caselle gerate
+  const cellsArray = [];
   const clickedCells = [];
 
   //Genero tutto in base alla dificolta'
@@ -49,12 +51,13 @@ function generateGrid() {
     cellElement.classList.add('cell');
     cellElement.innerHTML = num
     containerElement.append(cellElement);
+    cellsArray.push(cellElement);
 
     //Aggiungo la funzionalita' di cambio colore alle celle
     cellElement.addEventListener('click', function () {
       if (bombs.includes(num) == true && isGameOver == false) {
         this.classList.toggle('bg-danger')
-        gameOver();
+        gameOver(score, maxScore, bombs, cellsArray);
       } else if (clickedCells.includes(num) == false && isGameOver == false) {
         this.classList.toggle('bg-primary');
         clickedCells.push(num)
@@ -67,9 +70,13 @@ function generateGrid() {
   }
 }
 
-function gameOver() {
+function gameOver(score, maxScore, bombs, cellsArray) {
   isGameOver = true;
-  
+  for (let i = 0; i < bombs.length; i++) {
+    let index2 = bombs[i];
+    cellsArray[index2].classList.add('bg-danger')
+  }
+  alert(`Game Over! Hai raggiunto un totale di ${score} punti su un massimo di ${maxScore}, riprova!`)
 }
 
 /* CODICE */
